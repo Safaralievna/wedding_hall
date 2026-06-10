7a193edcc5051bff8eabcb5d159ceb615209beb4
Safaralievna <farangizshaykulova06@gmail.com>
2026-06-10 16:17:06 +0500

    Fix code

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
